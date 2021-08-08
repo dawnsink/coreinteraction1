a21f8e7f84de83c5a32e9e2fd3a3ae1f8e5dd1dd
dawnsink <60200649+dawnsink@users.noreply.github.com>
2021-08-08 00:29:40 -0400

    initial commit

---
+++ homeroom/homreoom.js
@@ -0,0 +1,10 @@
+window.setInterval(function(){
+
+    var randomH = Math.floor(Math.random() * 360);
+  
+     $('body').css({
+   'background-color' : 'hsl(' + randomH + ', 100%, 70%)'
+  });
+  
+  }, 1000);
+  
